7d1f5a24795d7d4ac96731b11a2ad3ce1dc43041
Cornelius Appia-Kubi <appiakubi4u@yahoo.com>
2025-08-20 09:29:13 +0000

    Add files via upload

---
+++ pyhtongoals.py
@@ -0,0 +1,20 @@
+print("#============================================")
+print("#CORNELIUS-SNOWHYT Programming Ambitions=====")
+print("#============================================\n")
+
+print("Welcome to CORNELIUS-SNOWHYT's Python journey!\n")
+print("""
+This file contains my ambitions and learning path in Python.\n
+1. Continue to improving my Regex Data Validator project
+2. Build automation tools:
+   --File renamer
+   --Password generator
+   --Email/phone number/date and time/digital house address formatter
+3.Create a personal budget tracker program,
+4.Explore working with APIs (e.g., currency rates, weather) 
+5.Learn basics of Machine Learning and AI in the future.
+6.Ultimately, my goal is to become a Data Scientist.\n\n""")
+
+print("=====Staying consistent and keep building======")
+print("          DEO CONFIDIMUS              ")
+
